2880f8bd1a420576995e005b11aabef7029dcee9
Yuri Goulart <yurigoulart8@gmail.com>
2022-12-07 15:27:21 -0300

    terceira tela

---
+++ moviesApp/Controllers/Actors/ActorsViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class ActorsViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
